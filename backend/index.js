990d1dcf6b88b83f42ca4c18597195b1e2ff52a5
Abrar2003 <spartanabrar@gmail.com>
2023-04-09 18:22:55 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const config = require("./src/config.js");
 const UserRouter = require("./src/routers/user.router.js");
 const PostsRouter = require("./src/routers/posts.router.js");
@@ -8,6 +9,7 @@ const AnalyticsRouter = require("./src/routers/analytics.router.js");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/users",UserRouter);
 app.use("/posts", PostsRouter);
 app.use("/analytics", AnalyticsRouter);
